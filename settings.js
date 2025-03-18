b771f3029062a915f9262d52f9d882f3a58a35f6
Ism401 <shakaismail359@gmail.com>
2025-03-18 22:58:27 +0100

    b

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Maduk Serial', // Your name
-  ownerNumber: '09112747649', //Your number
+  ownerNumber: '9112747649', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
